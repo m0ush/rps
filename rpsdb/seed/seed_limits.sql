d5689d31a4f320a4e523b8db52862caa9a317bff
m0ush <48460165+m0ush@users.noreply.github.com>
2021-04-27 12:45:02 -0400

    change db table name and add day lag field

---
+++ rpsdb/seed/seed_limits.sql
@@ -0,0 +1,4 @@
+INSERT INTO limits (trigger_id, value, name, day_lag)
+VALUES
+    (1, -30, 'yesterday', 1),
+    (2, -50, 'last_week', 4);
--- rpsdb/seed/seed_thresholds.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO thresholds (trigger_id, value, name)
-VALUES
-    (1, -30, 'yesterday'),
-    (2, -50, 'last_week');
